892c924b6a5f161a3b28197e187a2ddd65c737ae
nawelGl <ghazal.nawel@gmail.com>
2025-12-13 00:18:54 +0100

    initial commit - project creation

---
+++ watch-dog/src/main/java/fr/upec/episen/tp/paas/watch_dog/WatchDogApplication.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.tp.paas.watch_dog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchDogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchDogApplication.class, args);
+	}
+
+}
---
+++ watch-dog/src/test/java/fr/upec/episen/tp/paas/watch_dog/WatchDogApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.upec.episen.tp.paas.watch_dog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchDogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
